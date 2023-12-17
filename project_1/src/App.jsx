3f71c76e3a361e1b00662d7d7d78dc099285f25c
Prashant Kumar <prashantkumar0507200@gmail.com>
2023-12-17 13:31:42 +0530

    project 1 react

---
+++ project_1/src/App.jsx
@@ -0,0 +1,12 @@
+import Navigation from "./components/navigation";
+import Hero from "./components/hero";
+function App() {
+  return ( 
+    <div>
+      <Navigation />
+      <Hero />
+    </div>
+   );
+}
+
+export default App
