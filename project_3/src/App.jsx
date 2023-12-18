572fc100ad3343c5fc6c5dbdc52fee6baeac9924
Prashant Kumar <prashantkumar0507200@gmail.com>
2023-12-18 23:06:48 +0530

    project 2 reame

---
+++ project_3/src/App.jsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const Button = styled.button`
+background-color: red;
+color: white;
+padding: 10px;
+`
+
+export default function App() {
+  return <>
+        <Button>This is Styled component Button</Button>
+        </>;
+}
